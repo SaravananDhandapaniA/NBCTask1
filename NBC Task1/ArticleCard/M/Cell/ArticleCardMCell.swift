65e3cc2965825d1f89e53af5111284cfb8448d57
SaravananDhandapaniA <saravanan@tringapps.com>
2022-06-01 19:28:44 +0530

    Constraints changes in XL and M cards

--- NBC Task1/ArticleCard/M/Cell/ArticleCardMCell.swift
+++ NBC Task1/ArticleCard/M/Cell/ArticleCardMCell.swift
@@ -10,13 +10,21 @@ import UIKit
 class ArticleCardMCell: UITableViewCell {
 
     
-    @IBOutlet weak var cardView: UIView!
-    @IBOutlet weak var mTitleSummary: UIView!
-    @IBOutlet weak var imageM: UIImageView!
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var summaryView: UIView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var timeStampLabel: UILabel!
+    @IBOutlet weak var ImageM: UIImageView!
     
+//
+//    @IBOutlet weak var cardView: UIView!
+//    @IBOutlet weak var mTitleSummary: UIView!
+//    @IBOutlet weak var imageM: UIImageView!
+//    @IBOutlet weak var nameLabel: UILabel!
+//    @IBOutlet weak var titleLabel: UILabel!
+//    @IBOutlet weak var timeStampLabel: UILabel!
+//
     static var cellIdentifier = "ArticleCardMCell"
     
     
@@ -32,7 +40,7 @@ class ArticleCardMCell: UITableViewCell {
     }
     func configureDataForM(_ data:ContextItem? , _ image:URL)
     {
-        imageM.af_setImage(withURL: image)
+        ImageM.af_setImage(withURL: image)
         nameLabel.text = data?.eyebrow?.label?.uppercased()
         titleLabel.text = data?.title
         timeStampLabel.text = data?.dateString
